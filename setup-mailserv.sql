6fd93e4547691efc508a7516e12a082ab7de0f60
XtremXpert <benoit@vezina.biz>
2016-04-21 12:06:42 -0400

    Update setup-mailserv.sql

--- setup-mailserv.sql
+++ setup-mailserv.sql
@@ -1,10 +1,13 @@
 GRANT SELECT ON mailserver.* TO 'mailuser'@'127.0.0.1' IDENTIFIED BY 'mailuserpass';
+
 FLUSH PRIVILEGES;
+
 CREATE TABLE `virtual_domains` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `virtual_users` (
   `id` int(11) NOT NULL auto_increment,
   `domain_id` int(11) NOT NULL,
@@ -14,6 +17,7 @@ CREATE TABLE `virtual_users` (
   UNIQUE KEY `email` (`email`),
   FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `virtual_aliases` (
   `id` int(11) NOT NULL auto_increment,
   `domain_id` int(11) NOT NULL,
@@ -22,6 +26,7 @@ CREATE TABLE `virtual_aliases` (
   PRIMARY KEY (`id`),
   FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 INSERT INTO `mailserver`.`virtual_domains`
   (`id` ,`name`)
 VALUES
